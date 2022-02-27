ca9d1f59f6dc806bc1dcd5d8a95720b8a9761233
mpramos <mpramos78915400@gmail.com>
2022-02-27 04:55:18 -0400

    Property Binding/ COnfigura las propiedades de elementos html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,12 @@ export class AppComponent {
   name = 'Maria';
   age = 18;
   img = "https://phantom-marca.unidadeditorial.es/8d51e0bba2ea50747296c499719750b9/resize/660/f/webp/assets/multimedia/imagenes/2021/08/11/16286724786905.jpg"
+  btnDisabled = true
+
+  persona = {
+    name: 'Selena Gomez',
+    age : 29,
+    avatar :"https://phantom-marca.unidadeditorial.es/8d51e0bba2ea50747296c499719750b9/resize/660/f/webp/assets/multimedia/imagenes/2021/08/11/16286724786905.jpg"
+  }
 
 }
